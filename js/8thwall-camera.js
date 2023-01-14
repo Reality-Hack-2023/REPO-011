79a965927f88374437879759e60e045abf73d556
Ashish Patel <apatel.developer@gmail.com>
2023-01-14 11:17:28 -0500

    initialized

---
+++ js/8thwall-camera.js
@@ -0,0 +1,152 @@
+/**
+ * [WIP]
+ * New 8thwall camera component
+ * Currently only works with BACK camera.
+ * Suffers greatly from Out-of-memory on safari iOS. If it starts crashing - you have to actually close and reopend safari
+ *
+ * How to use:
+ * - Create an empty project in the WL editor
+ * - Make sure the AR is disabled in the project settings (this avoids clashing with the old 8thwall component)
+ * - Add a ViewComponent to the scene
+ * - Add this Component to the View
+ */
+
+/**
+ * Sets up the 8thwall pipeline and retrieves tracking events to place an
+ * object at the location of the tracked AR camera / mobile device.
+ *
+ * Use this for SLAM tracking based on 8thwall.
+ *
+ * <strike>Make sure to enable 8thwall in "Project Settings" > "AR". See also the
+ * [AR Getting Started Guide](/getting-started/quick-start-ar)</strike>.
+ * */
+
+
+
+// TODO: leave this to be generated in the index.html by the editor
+const KEY = "mzHVlEGqWxbqx6BBVxHZ9j61wLrZNIUqviX8C7i38B0w15Zr7j7V4Zy2YMvjGx8MJhTwig";
+function loadXR8(scriptURL) {
+    return new Promise((res, rej) => {
+        const s = document.createElement('script');
+
+        window.addEventListener('xrloaded', () => res(s));
+        s.onerror = rej;
+        s.setAttribute("crossorigin", "anonymous");
+        s.src = scriptURL;
+        document.body.appendChild(s);
+    });
+}
+
+WL.registerComponent('8thwall-camera0', {}, {
+    name: 'wonderland-engine-8thwall-camera',
+
+    started: false,
+
+    view: null, // cache camera
+
+    position: [0, 0, 0], // cache 8thwall cam position
+
+    rotation: [0, 0, 0, -1], // cache 8thwall cam rotation
+
+    GlTextureRenderer: null, // cache XR8.GlTextureRenderer.pipelineModule
+
+    init: function () {
+        this.view = this.object.getComponent("view");
+
+        this.onUpdate = this.onUpdate.bind(this);
+        this.onAttach = this.onAttach.bind(this);
+    },
+
+    start: async function () {
+
+        WL.scene.colorClearEnabled = false;
+
+        // TODO: retrieve the API key from the editor
+        await loadXR8("//apps.8thwall.com/xrweb?appKey=" + KEY);
+
+        XR8.XrController.configure({
+            // enableLighting: true,
+            disableWorldTracking: false
+        });
+
+        this.GlTextureRenderer = XR8.GlTextureRenderer.pipelineModule();
+        XR8.addCameraPipelineModules([
+            this.GlTextureRenderer, // Draws the camera feed.
+            XR8.XrController.pipelineModule(), // Enables SLAM tracking.
+            this
+        ]);
+
+        const config = {
+            cameraConfig: {
+                direction: XR8.XrConfig.camera().BACK,
+            },
+            canvas: Module.canvas,
+            allowedDevices: XR8.XrConfig.device().ANY,
+            ownRunLoop: false,
+        }
+
+        XR8.run(config);
+    },
+
+    onAttach: function (params) {
+        this.started = true;
+
+        // TODO: is there a better way to retrieve the context? Maybe from WL directly?
+        const gl = WL.canvas.getContext("webgl2");
+
+        const rot = this.object.rotationWorld;
+        const pos = this.object.getTranslationWorld([]);
+        this.position = Array.from(pos);
+        this.rotation = Array.from(rot);
+
+        XR8.XrController.updateCameraProjectionMatrix({
+            origin: { x: pos[0], y: pos[1], z: pos[2] },
+            facing: { x: rot[0], y: rot[1], z: rot[2], w: rot[3] },
+
+            // TODO: should we include this? Does not have any effect, threejs pipeline does not use it. But Babylon does
+            // cam: { pixelRectWidth: Module.canvas.width, pixelRectHeight: Module.canvas.height, nearClipPlane: 0.01, farClipPlane: 100 }
+        })
+
+        WL.scene.onPreRender.push(() => {
+            gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null); // <--- Should not be needed after next nightly is released (current 20230110)
+            XR8.runPreRender(Date.now());
+
+            XR8.runRender(); // <--- tell 8thwall to do it's thing (alternatively call this.GlTextureRenderer.onRender() if you only care about camera feed )
+        });
+
+        WL.scene.onPostRender.push(() => {
+            XR8.runPostRender(Date.now())
+        });
+    },
+
+
+    onUpdate: function (e) {
+        if (!e.processCpuResult.reality)
+            return;
+
+        const { rotation, position, intrinsics } = e.processCpuResult.reality;
+
+        this.rotation[0] = rotation.x;
+        this.rotation[1] = rotation.y;
+        this.rotation[2] = rotation.z;
+        this.rotation[3] = rotation.w;
+
+        this.position[0] = position.x;
+        this.position[1] = position.y;
+        this.position[2] = position.z;
+
+        if (intrinsics) {
+            for (let i = 0; i < 16; i++) {
+                if (Number.isFinite(intrinsics[i])) { // some processCpuResult.reality.intrinsics are set to Infinity, which WL brakes our projectionMatrix. So we just filter those elements out
+                    this.view.projectionMatrix[i] = intrinsics[i];
+                }
+            }
+        }
+
+        if (position && rotation) {
+            this.object.resetTransform();
+            this.object.rotate(this.rotation);
+            this.object.translate(this.position);
+        }
+    },
+});
---
+++ js/firestore-api.js
@@ -0,0 +1,53 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc, getDoc, updateDoc, doc, FieldValue, arrayUnion } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADwKiflDgXfEiSPe0Oome7ivlgxsNKTcc",
+  authDomain: "mitreality.firebaseapp.com",
+  databaseURL: "https://mitreality-default-rtdb.firebaseio.com",
+  projectId: "mitreality",
+  storageBucket: "mitreality.appspot.com",
+  messagingSenderId: "895952725895",
+  appId: "1:895952725895:web:b40a2574c470a1fe6d1522",
+  measurementId: "G-LR2JTML4BT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getPosts(db) {
+  const posts = collection(db, 'posts');
+  const postsSnapshot = await getDocs(posts);
+  const postsList = postsSnapshot.docs.map(doc => doc.data());
+  return postsList;
+}
+
+async function newPost(db, text){
+  const postRef = await addDoc(collection(db, "posts"), {
+    text: text,
+    likes: 0,
+    comments: []
+  }).then(function(postRef) {
+      console.log("Document written with ID: ", postRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+}
+
+async function newComment(db, postDoc, commentText){
+  const posts = doc(db, `posts/${postDoc}`);
+  let postsSnapshot = await getDoc(posts);
+  const unionRes = await updateDoc(posts, {
+    comments: arrayUnion(commentText)
+  });
+  postsSnapshot = await getDoc(posts);
+  return postsSnapshot.get('comments')
+}
