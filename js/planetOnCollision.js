e24b98f0063c6f6c8083c60df21b178b54b254cb
Akash <anallani@umich.edu>
2023-01-14 22:48:38 -0500

    changed hover behavior to click

--- js/planetOnCollision.js
+++ js/planetOnCollision.js
@@ -9,23 +9,30 @@ WL.registerComponent('planetOnCollision', {
     },
     start: function() {
         var cursor = this.object.getComponent("cursor-target");
-        cursor.addHoverFunction( o=>{
-            var newMesh = this.object.children[0].children[0].children[0].children[0].getComponent("mesh");
-            newMesh.material = this.material_change;
-            var allInactiveButtons = document.querySelectorAll(".inactive_button");
-            allInactiveButtons.forEach(element => {
-                element.className = "active_button";
-                element.disabled = false;
-            });
-        })
-        cursor.addUnHoverFunction( o=>{
-            var newMesh = this.object.children[0].children[0].children[0].children[0].getComponent("mesh");
-            newMesh.material = this.material_org;
-            var allActiveButtons = document.querySelectorAll(".active_button");
-            allActiveButtons.forEach(element => {
-                element.className = "inactive_button";
-                element.disabled = true;
-            });
+        var selected = false;
+        cursor.addClickFunction( o=>{
+            console.log("entered click function");
+            if (!selected) {
+                console.log("selected");
+                var newMesh = this.object.children[0].children[0].children[0].children[0].getComponent("mesh");
+                newMesh.material = this.material_change;
+                var allInactiveButtons = document.querySelectorAll(".inactive_button");
+                allInactiveButtons.forEach(element => {
+                    element.className = "active_button";
+                    element.disabled = false;
+                });
+                selected = true;
+            } else {
+                console.log("not selected");
+                var newMesh = this.object.children[0].children[0].children[0].children[0].getComponent("mesh");
+                newMesh.material = this.material_org;
+                var allActiveButtons = document.querySelectorAll(".active_button");
+                allActiveButtons.forEach(element => {
+                    element.className = "inactive_button";
+                    element.disabled = true;
+                });
+                selected = false;
+            }
         })
     },
     update: function(dt) {
